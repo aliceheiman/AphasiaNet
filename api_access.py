1322037ee6e77f1bf66217c8c135f940b2036164
Alice Heiman <a@heiman.se>
2025-04-05 12:50:58 -0400

    add code for api access speech to text

--- api_access.py
+++ api_access.py
@@ -6,17 +6,27 @@ client = ElevenLabs(
     api_key=os.getenv("ELEVENLABS_API_KEY"),
 )
 
-audio_stream = client.text_to_speech.convert_as_stream(
-    text="We are helping Aphasia patients regain their independence",
-    voice_id="JBFqnCBsd6RMkjVDRZzb",
-    model_id="eleven_multilingual_v2",
-)
+# audio_stream = client.text_to_speech.convert_as_stream(
+#     text="We are helping Aphasia patients regain their independence",
+#     voice_id="JBFqnCBsd6RMkjVDRZzb",
+#     model_id="eleven_multilingual_v2",
+# )
 
-stream(audio_stream)
+# stream(audio_stream)
 
 
 # speech to text
 
-client.speech_to_text.convert(
-    model_id="model_id",
+# text = client.speech_to_text.convert(
+#     file=os.path.join("audio", "aphasia-1-speaker.mp3"),
+#     model_id="scribe_v1",
+# )
+# print(text)
+
+file_path = os.path.join("audio", "aphasia-1-speaker.mp3")
+file_data = open(file_path, "rb")
+result = client.speech_to_text.convert(
+    model_id="scribe_v1",
+    file=file_data,
 )
+print(result)
